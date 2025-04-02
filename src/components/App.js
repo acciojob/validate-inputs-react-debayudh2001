9b52cc58bc179821765692ddc74a5751f0cca918
debayudh2001 <debayudh31@gmail.com>
2025-04-03 00:53:38 +0530

    third commit

--- src/components/App.js
+++ src/components/App.js
@@ -80,22 +80,22 @@ const App = () => {
         <label htmlFor="name">Name:</label>&nbsp;&nbsp;
         <input type="text" name="name" value={details.name} onChange={handleChange} />
         <br />
-        <span className={error.name ? "errorMessage" : "hide"}>{error.name}</span>
+        <span className="errorMessage">{error.name}</span>
         <br />
         <label htmlFor="address">Address:</label>&nbsp;&nbsp;
         <input type="text" name="address" value={details.address} onChange={handleChange} />
         <br />
-        <span className={error.address ? "errorMessage" : "hide"}>{error.address}</span>
+        <span className="errorMessage">{error.address}</span>
         <br />
         <label htmlFor="email">Email:</label>&nbsp;&nbsp;
         <input type="text" name="email" value={details.email} onChange={handleChange} />
         <br />
-        <span className={error.email ? "errorMessage" : "hide"}>{error.email}</span>
+        <span className="errorMessage">{error.email}</span>
         <br />
         <label htmlFor="mobile">Mobile:</label>&nbsp;&nbsp;
         <input type="text" name="mobile" value={details.mobile} onChange={handleChange} />
         <br />
-        <span className={error.mobile ? "errorMessage" : "hide"}>{error.mobile}</span>
+        <span className="errorMessage">{error.mobile}</span>
         <br />
         <button type="submit">Submit</button>
       </form>
